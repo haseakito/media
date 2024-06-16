2bb019077b4c178dc0120f653dd34d9b67c6c9a8
haseakito <haseakito@gmail.com>
2024-06-16 15:52:52 +0900

    :card_file_box: Add `Email Verification Token` schema

--- db/schema.ts
+++ db/schema.ts
@@ -1,19 +1,24 @@
 import {
-    mysqlTable,
-    varchar,
-    text,
-    bigint,
-    boolean,
-    date,
-    timestamp,
+  mysqlTable,
+  varchar,
+  text,
+  bigint,
+  boolean,
+  date,
+  timestamp,
+  index,
 } from "drizzle-orm/mysql-core";
-import { createId } from '@paralleldrive/cuid2';
+import { createId } from "@paralleldrive/cuid2";
 
-export const users = mysqlTable('users', {
-    id: varchar("id", { length: 255 }).primaryKey().$defaultFn(() => createId()),
+export const users = mysqlTable(
+  "users",
+  {
+    id: varchar("id", { length: 255 })
+      .primaryKey()
+      .$defaultFn(() => createId()),
     name: varchar("name", { length: 30 }).notNull(),
     email: varchar("email", { length: 255 }).notNull().unique(),
-    emailVerified: boolean("email_verified").default(false),
+    emailVerified: boolean("email_verified").notNull().default(false),
     password: varchar("password", { length: 255 }).notNull(),
     bio: text("bio"),
     link: varchar("link", { length: 255 }),
@@ -21,16 +26,41 @@ export const users = mysqlTable('users', {
     profileImage: varchar("profile_image", { length: 255 }),
     coverImage: varchar("cover_image", { length: 255 }),
     createdAt: timestamp("created_at").defaultNow().notNull(),
-    updatedAt: timestamp("updated_at").defaultNow().notNull().onUpdateNow()
-});
+    updatedAt: timestamp("updated_at").defaultNow().notNull().onUpdateNow(),
+  },
+  (table) => {
+    return {
+      emailIdx: index("email_idx").on(table.email),
+    };
+  }
+);
 
 export const sessions = mysqlTable("sessions", {
-    id: varchar("id", { length: 127 }).primaryKey(),
+  id: varchar("id", { length: 127 }).primaryKey(),
+  user_id: varchar("user_id", { length: 255 }).references(() => users.id),
+  activeExpires: bigint("active_expires", {
+    mode: "number",
+  }).notNull(),
+  idleExpires: bigint("idle_expires", {
+    mode: "number",
+  }).notNull(),
+});
+
+export const email_verification_token = mysqlTable(
+  "email_verification_table",
+  {
+    id: varchar("id", { length: 255 })
+      .primaryKey()
+      .$defaultFn(() => createId()),
+    code: varchar("code", { length: 8 }).notNull(),
     user_id: varchar("user_id", { length: 255 }).references(() => users.id),
-    activeExpires: bigint("active_expires", {
-		mode: "number"
-	}).notNull(),
-	idleExpires: bigint("idle_expires", {
-		mode: "number"
-	}).notNull()
-})
+    email: varchar("email", { length: 255 }).notNull().unique(),
+    expiresAt: timestamp("expires_at"),
+  },
+  (table) => {
+    return {
+      userIdIdx: index("user_id_idx").on(table.user_id),
+      emailIdx: index("email_idx").on(table.email),
+    };
+  }
+);
