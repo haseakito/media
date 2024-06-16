2396586cce10c011a3d000ca487cfce15318e883
haseakito <haseakito@gmail.com>
2024-06-16 23:06:34 +0900

    :card_file_box: Add `Not Null` constraints on columns

--- db/schema.ts
+++ db/schema.ts
@@ -13,9 +13,7 @@ import { createId } from "@paralleldrive/cuid2";
 export const users = mysqlTable(
   "users",
   {
-    id: varchar("id", { length: 255 })
-      .primaryKey()
-      .$defaultFn(() => createId()),
+    id: varchar("id", { length: 255 }).primaryKey(),
     name: varchar("name", { length: 30 }).notNull(),
     email: varchar("email", { length: 255 }).notNull().unique(),
     emailVerified: boolean("email_verified").notNull().default(false),
@@ -37,7 +35,7 @@ export const users = mysqlTable(
 
 export const sessions = mysqlTable("sessions", {
   id: varchar("id", { length: 127 }).primaryKey(),
-  user_id: varchar("user_id", { length: 255 }).references(() => users.id),
+  user_id: varchar("user_id", { length: 255 }).references(() => users.id).notNull(),
   activeExpires: bigint("active_expires", {
     mode: "number",
   }).notNull(),
@@ -53,9 +51,9 @@ export const email_verification_token = mysqlTable(
       .primaryKey()
       .$defaultFn(() => createId()),
     code: varchar("code", { length: 8 }).notNull(),
-    user_id: varchar("user_id", { length: 255 }).references(() => users.id),
+    user_id: varchar("user_id", { length: 255 }).references(() => users.id).notNull(),
     email: varchar("email", { length: 255 }).notNull().unique(),
-    expiresAt: timestamp("expires_at"),
+    expiresAt: timestamp("expires_at").notNull(),
   },
   (table) => {
     return {
