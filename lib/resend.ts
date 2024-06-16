36f9994b43e7cb2e801874d9b9b2b2072b36b71e
haseakito <haseakito@gmail.com>
2024-06-16 23:26:28 +0900

    :sparkles: Add send verification email

--- lib/resend.ts
+++ lib/resend.ts
@@ -1,4 +1,5 @@
 import { Resend } from "resend";
+import VerifyEmailTemplate from "@/emails/verify-email";
 
 export const resend = new Resend(process.env.RESEND_API_KEY);
 
@@ -7,3 +8,27 @@ interface sendVerificationEmailProps {
   email: string;
   verificationCode: string;
 }
+
+export const sendVerificationEmail = async ({
+  name,
+  email,
+  verificationCode,
+}: sendVerificationEmailProps) => {
+  // Create a new email verification link
+  const verificationLink = `http://${process.env.NEXT_PUBLIC_API_URL}/auth/verify?token=${verificationCode}`;
+
+  // Send a email verification email
+  try {
+    await resend.emails.send({
+      from: "noreply@hackgame.biz",
+      to: [email],
+      subject: "Verify your email",
+      react: VerifyEmailTemplate({ name, verificationLink }),
+      headers: {
+        "X-Entity-Ref-ID": new Date().getTime() + "",
+      },
+    });
+  } catch (error) {
+    console.log("Error while sending an email: ", error);
+  }
+};
