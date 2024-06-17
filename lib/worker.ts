8678de7ba8fd19a419300488dbc52f8cbc3c6469
haseakito <haseakito@gmail.com>
2024-06-18 02:46:35 +0900

    :sparkles: Add send password reset email

--- lib/worker.ts
+++ lib/worker.ts
@@ -1,13 +1,24 @@
 import { Job } from "bullmq";
-import { sendVerificationEmail } from "./resend";
+import { sendPasswordResetEmail, sendVerificationEmail } from "./resend";
 
-/**
- *
- */
 export const emailProcessors = {
+  /**
+   * Sends a verification email to a user
+   *
+   * @param job The job containing the user data
+   */
   sendVerificationEmail: async (job: Job) => {
     const { name, email, verificationCode } = job.data;
     await sendVerificationEmail({ name, email, verificationCode });
   },
-  sendPasswordResetEmail: async (job: Job) => {},
+
+  /**
+   * Sends a password reset email to a user
+   *
+   * @param job The job containing the user data
+   */
+  sendPasswordResetEmail: async (job: Job) => {
+    const { name, email, verificationToken } = job.data;
+    await sendPasswordResetEmail({ name, email, verificationToken });
+  },
 };
